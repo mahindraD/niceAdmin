ce98d87cf5ba4c2e8f7c38ab12c79b94705c77b5
mahindraD <d.mahindra04010611@gmail.com>
2024-11-10 22:21:13 +0530

    icons added

--- mahindra.js
+++ mahindra.js
@@ -90,12 +90,16 @@ if ("content" in document.createElement("template")) {
   // Clone the new row and insert it into the table
   const clone = template.content.cloneNode(true); 
 
+  let icons = ["text-success","text-danger","text-primary","text-info","text-warning","text-muted"]
+  let i=0;
+  
   array.forEach(element => {
     const listBody = clone.querySelector(".card .card-body .activity");
     const tt = document.querySelector("#list");
     const cc = tt.content.cloneNode(true);
     cc.querySelector(".activity-item .activite-label").innerText=element.label;
     cc.querySelector(".activity-item .activity-content").innerText=element.content;
+    cc.querySelector(".activity-badge").classList.add(icons[(i++)%6]);
     listBody.appendChild(cc);
   });
 
@@ -111,6 +115,6 @@ revenue_func(180);
 customer_func(789);
 // recentActivity();
 
-array = [{label:"7min",content:"mumbai indians"},{label:"25min",content:"csk"},{label:"45min",content:"mumbai indians"},{label:"50min",content:"csk"}]
+array = [{label:"7 min",content:"mumbai indians"},{label:"40 min",content:"csk"},{label:"2 days",content:"mumbai indians"},{label:"1 week",content:"csk"},{label:"4 weeks",content:"mumbai indians"}];
 recentActivity_copy(array);
 
