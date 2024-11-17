4655a67074350cd38efb214740774785ebff736c
mahindraD <d.mahindra04010611@gmail.com>
2024-11-17 20:48:24 +0530

    compnent created

--- mahi.js
+++ mahi.js
@@ -54,4 +54,78 @@ array = [
 
 ]
 // chartCardFunc("rohit",array);
-chartCardOnly("rahul");
+// chartCardOnly("rahul");
+
+'use strict';
+  class ChatCard extends HTMLElement {
+    constructor() {
+      super();
+
+
+      const title = "rahul";
+      const array = [
+        {message:"Hi Alfy, how can i help you?",left:false},
+        {message:"I'm looking for the best admin dashboard template",left:true},
+        {message:"With bootstrap certainly",left:true},
+        {message:"I recommend you to use Mazer Dashboard",left:false},
+        {message:"That's great! I like it so much :)",left:true},
+      ]
+
+      this.innerHTML = `
+        <div class="card-header">
+                <div class="media d-flex align-items-center">
+                    <div class="avatar me-3">
+                        <img src="./assets/compiled/jpg/1.jpg" alt="" srcset="">
+                        <span class="avatar-status bg-success"></span>
+                    </div>
+                    <div class="name flex-grow-1">
+                        <h6 class="mb-0">${title}</h6>
+                        <span class="text-xs">Online</span>
+                    </div>
+                    <button class="btn btn-sm">
+                        <i data-feather="x"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card-body pt-4 bg-grey">
+              <div class="chat-content">
+                    ${array.map(item=>{
+                      if(item.left){
+                        return `
+                          <div class="chat chat-left">
+                            <div class="chat-body">
+                              <div class="chat-message">
+                                ${item.message}
+                              </div>
+                            </div>
+                          </div>
+                        `
+                      }
+                      else{
+                        return `
+                          <div class="chat">
+                            <div class="chat-body">
+                              <div class="chat-message">
+                                ${item.message}
+                              </div>
+                            </div>
+                          </div>
+                        `
+                      }
+                    }).join("")}
+              </div>
+            </div>
+            <div class="card-footer">
+                <div class="message-form d-flex flex-direction-column align-items-center">
+                    <a href="http://" class="black"><i data-feather="smile"></i></a>
+                    <div class="d-flex flex-grow-1 ms-4">
+                        <input type="text" class="form-control" placeholder="Type your message..">
+                    </div>
+                </div>
+            </div>
+      `;
+    }
+  }
+
+  // let the browser know about the custom element
+customElements.define('chat-card', ChatCard);
